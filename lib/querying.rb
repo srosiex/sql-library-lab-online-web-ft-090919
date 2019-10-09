6baedbc8300d67e1ab3ea37364688ffec8407619
Sarah Henderson <sarahrose.henderson@gmail.com>
2019-10-08 19:23:25 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,58 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "
+  SELECT books.title, books.year
+  FROM series
+  JOIN books on series.id = books.series_id
+  WHERE books.series_id = 1
+  ORDER BY books.year
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "
+  SELECT characters.name, MAX(characters.motto)
+  FROM characters
+  GROUP BY characters.motto LIMIT 1
+
+  "
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "
+  SELECT characters.species, COUNT(*)
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY characters.species DESC LIMIT 1
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "
+  SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series on authors.id = series.author_id
+  JOIN subgenres on series.subgenre_id = subgenres.id
+  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "
+  SELECT series.title
+  FROM series
+  JOIN authors on series.author_id = authors.id
+  JOIN characters on characters.author_id = authors.id
+  WHERE characters.species = 'human'
+  GROUP BY series.title LIMIT 1
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "
+  SELECT characters.name, COUNT(*) AS book_count
+  FROM characters
+  JOIN character_books on characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY book_count DESC
+  "
 end
